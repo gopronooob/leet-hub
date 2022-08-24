75ab6d4a8c5a0ef96bedd27b1a8843a582c5ed27
Akshay Pendbhaje <akshay.pendbhaje@gmail.com>
2022-08-24 21:42:29 +0530

    Time: 124 ms (19.40%), Space: 47.4 MB (46.84%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -25,19 +25,35 @@ class Solution {
     }
     
 private ListNode mergeLists(ListNode list1, ListNode list2) {
-		if (list1 == null)
-			return list2;
-		if (list2 == null)
-			return list1;
-
-    //System.out.println(list1.val +" "+ list2.val);
-		if (list1.val < list2.val) {
-			list1.next = mergeLists(list1.next, list2);
-			return list1;
-		} else {
-			list2.next = mergeLists(list2.next, list1);
-			return list2;
-		}
-	}
-
+    if(list1 == null)
+        return list2;
+    if(list2==null)
+        return list1;
+    
+    ListNode ans=new ListNode();
+    ListNode walker=ans;
+    while(list1!=null &&list2 !=null)
+    {
+        if(list1.val<list2.val)
+        {
+            walker.next=list1;
+            walker=walker.next;
+            list1=list1.next;            
+        }
+        else
+        {
+            walker.next=list2;
+            walker=walker.next;
+            list2=list2.next;
+        }
+        
+        if(list1==null)
+            walker.next=list2;
+        if(list2==null)
+            walker.next=list1;
+        
+    }
+    
+        return ans.next;
+}
 }
