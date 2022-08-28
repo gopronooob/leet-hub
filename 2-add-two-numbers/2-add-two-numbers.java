e8a048435137307c5533e527d217956462829f68
Akshay Pendbhaje <akshay.pendbhaje@gmail.com>
2022-08-28 23:47:01 +0530

    Time: 4 ms (37.59%), Space: 46.9 MB (84.38%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -10,65 +10,24 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) { 
-        
-        if(l1==null) 
-            return l2;
-        if(l2==null)
-            return l1;
-        ListNode ans=null;
-        ListNode walker=ans;
-        int carry=0;
-        
-        while(l1!=null &&l2!=null)
+      int num1=0,num2=0,sum=0,carry=0;
+        ListNode ans=new ListNode();
+        ListNode dummy=ans;
+       
+        while((l1!=null || l2!=null) || carry==1)
         {
+            num1=(l1!=null)?l1.val:0;
+            num2=(l2!=null)?l2.val:0;
             
-            int sum=l1.val+l2.val+carry;
-            carry=(sum<10)?0:1;
-            sum=sum%10;
-            if(ans==null)
-            {
-                ans=new ListNode(sum); 
-                walker=ans;
-            }
-            else
-            {
-                ListNode temp=new ListNode(sum); 
-                walker.next=temp;
-                walker=walker.next;
-            }
-            l1.val=sum;
-            l1=l1.next;
-            l2=l2.next;
-        }
-        
-        while(l1!=null)
-        {
-            int sum=l1.val+carry;
-            carry=(sum<10)?0:1;
-            sum=sum%10;
-            ListNode temp=new ListNode(sum); 
-            l1=l1.next;
-            walker.next=temp;
-            walker=walker.next;            
-            
-        }
-        while(l2!=null)
-        {
-            int sum=l2.val+carry;
-            carry=(sum<10)?0:1;
-            sum=sum%10;
-            ListNode temp=new ListNode(sum); 
-            l2=l2.next;
-            walker.next=temp;
-            walker=walker.next;            
-            
-        }  
-        if(carry!=0)
-        {
-            ListNode temp=new ListNode(carry);
-            walker.next=temp;
-            
-        }
-        return ans;
+              sum=num1+num2+carry;
+            ans.next=new ListNode(sum%10);
+            ans=ans.next;
+            carry=sum/10;
+            if(l1!=null)
+                l1=l1.next;
+            if(l2!=null)
+                l2=l2.next;
+        } 
+        return dummy.next;
     }
 }
