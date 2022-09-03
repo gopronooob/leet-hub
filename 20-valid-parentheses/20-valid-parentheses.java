04316708ad65b6ecdc5bf80fb5b7082f67fe4d72
Akshay Pendbhaje <akshay.pendbhaje@gmail.com>
2022-09-03 21:06:00 +0530

    Time: 1 ms (99.63%), Space: 40.1 MB (96.18%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+         
+        for(char ch:s.toCharArray())
+        {
+            if(ch=='(' || ch=='{'|| ch=='[')
+            {
+                stack.push(ch);
+            }
+            else
+            {
+                if(ch==')' && !stack.empty() &&stack.peek()=='(')
+                    stack.pop();
+                else if(ch=='}' && !stack.empty() &&stack.peek()=='{')
+                    stack.pop();
+                else if(ch==']' && !stack.empty() && stack.peek()=='[')
+                    stack.pop();
+                else 
+                return false;
+            }
+        }
+        return(stack.isEmpty());
+        
+    }
+}
