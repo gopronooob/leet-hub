ac3d10f92d2bd88b488692347988e8678d39e4a4
Akshay Pendbhaje <akshay.pendbhaje@gmail.com>
2022-09-21 22:20:46 +0530

    Time: 2 ms (66.86%), Space: 39.2 MB (95.15%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -8,17 +8,17 @@ class Solution {
             players.add(i+1);
         }
             
-        return winnerFinder(n,k,players,0);        
+        return winnerFinder(n,k-1,players,0);        
     }
     
     int  winnerFinder(int n,int k,ArrayList<Integer> players,int index)
     {
-        if(1==n)
+        if(1==players.size())
         {            
             return players.get(0);
         }   
-        index=(index+k-1)%n;
-        players.remove(players.get(index));
+        index=(index+k)%players.size(); 
+        players.remove(index);
        return winnerFinder(n-1,k,players,index); 
            
     }
