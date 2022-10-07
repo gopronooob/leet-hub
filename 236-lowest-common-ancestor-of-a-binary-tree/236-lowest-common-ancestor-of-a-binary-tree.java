8d8b0383296c7631e4d95d1a7643f3c810328aba
Akshay Pendbhaje <akshay.pendbhaje@gmail.com>
2022-10-07 09:45:44 +0530

    Time: 13 ms (46.08%), Space: 47.5 MB (38.07%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,31 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
-        
-        if(root==null || root==p || root==q)
-            return root;
-        
-        TreeNode leftSearch=lowestCommonAncestor(root.left, p, q) ;
-        TreeNode rightSearch=lowestCommonAncestor(root.right, p, q) ;
-        
-        if(leftSearch!=null && rightSearch !=null)
-            return root;
-        
-        if(leftSearch!=null)
-            return leftSearch;
-        
-      //  if(rightSearch!=null)
-            return rightSearch;
-        
-        
+        if (root == null) 
+            return null; 
+  
+        // If either n1 or n2 matches with root's key, report 
+        // the presence by returning root (Note that if a key is 
+        // ancestor of other, then the ancestor key becomes LCA 
+        if (root.val == p.val || root.val == q.val) 
+            return root; 
+  
+        // Look for keys in left and right subtrees 
+        TreeNode left_lca = lowestCommonAncestor(root.left, p, q); 
+        TreeNode right_lca = lowestCommonAncestor(root.right, p, q); 
+  
+        // If both of the above calls return Non-NULL, then one key 
+        // is present in once subtree and other is present in other, 
+        // So this node is the LCA 
+        if (left_lca!=null && right_lca!=null) 
+            return root; 
+  
+        // Otherwise check if left subtree or right subtree is LCA 
+        return (left_lca != null) ? left_lca : right_lca; 
     }
 }
