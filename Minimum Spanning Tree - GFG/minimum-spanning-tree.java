2dd7e6a9a47c365e9e05276e9db3a7f60f5f9ec7
Akshay Pendbhaje <akshay.pendbhaje@gmail.com>
2022-10-21 10:39:21 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.java
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.java
@@ -53,40 +53,64 @@ class Solution
     //Function to find sum of weights of edges of the Minimum Spanning Tree.
     static int spanningTree(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj) 
     {
-        boolean visited[] = new boolean[V+1];
+        // Add your code here
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        int ans = 0;
+        int[] parent = new int[V+1];
         
-        PriorityQueue<Pair> pq = new PriorityQueue<>(); 
-        pq.add(new Pair(adj.get(0).get(0).get(0),0));
-        int ans=0;
+        for(int i=0;i<V;i++)
+        {
+            ArrayList<ArrayList<Integer>> edges= adj.get(i);
+            for(ArrayList<Integer> edge : edges)
+                pq.add(new Pair(i,edge.get(0),edge.get(1)));
+        }
+        
+        Arrays.fill(parent,-1);
         while(!pq.isEmpty())
         {
-            Pair current= pq.remove();
-            if(visited[current.src]) continue;
-            ans+=current.weight;
-            visited[current.src]=true;
-         //   System.out.println(current.src+" "+adj.get(current.src));
-            for(ArrayList<Integer> weight: adj.get(current.src))
-                if(!visited[weight.get(0)])
-                    pq.add(new Pair(weight.get(0),weight.get(1)));
+            Pair curr= pq.remove();
+            int srcParent = getParent(curr.src,parent);
+            int destParent = getParent(curr.dest,parent);
+            
+            if(srcParent!=destParent)
+            {
+                ans=ans+curr.weight;
+                
+                parent[srcParent]=destParent;
+            }
         }
+        
         return ans;
     }
+    
+static int getParent(int node,int [] parents){
+        if (parents[node]<0) return node;
+        
+        return  getParent(parents[node],parents);
+    }
+    
 }
 
 
-class Pair implements Comparable<Pair> 
-  {
-      int src; 
-      int weight;
-      
-      public Pair(int src , int weight)
-      {
-          this.src=src;
-          this.weight=weight; 
-      }
-      
-      public int compareTo(Pair other)
-      {
-          return this.weight-other.weight;
-      }
-  }
+class Pair implements Comparable<Pair>
+{
+    int src;
+    int weight;
+    int dest;
+    public Pair(int src,int dest,int weight)
+    {
+        this.src=src;
+        this.weight=weight;
+        this.dest=dest;
+    }
+    
+    public int compareTo(Pair other)
+    {
+        return this.weight-other.weight;
+    }
+    
+    public String toString()
+    {
+        return "src "+this.src +" dest "+ +this.dest+" weight "+weight;
+    }
+}
