28497b9f409679369793766303b1502a4aa2ddc0
Akshay Pendbhaje <akshay.pendbhaje@gmail.com>
2022-10-24 23:21:36 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -47,36 +47,38 @@ class gfg
 
 
 class Solution 
-{  static int cost[][];
+{ 
+        public static int[][] dp; 
     //Function to return max value that can be put in knapsack of capacity W.
     static int knapSack(int W, int wt[], int val[], int n) 
     { 
-
-        cost = new int[n + 1][W + 1];
-
-        for(int i=0;i<cost.length;i++){
-            for(int j =0;j < cost[i].length;j++)
-            cost[i][j] = -1;
-        }
-
-        return solve(W,wt,val,n);
-
+        
+           dp =  new int[W+1][n+1]; 
+        Arrays.stream(dp).forEach(a -> Arrays.fill(a, -1));
+        return helper(W,wt,val,n);
+  
+         
     } 
-
-    static int solve(int W, int[] wt, int val[], int n){
-
-        if(n == 0 || W == 0) return 0;
-
-        if(cost[n][W] != -1) return cost[n][W];
-
-        if(W >= wt[n - 1]){
-            return cost[n][W] = Math.max((val[n - 1] + solve(W - wt[n - 1], wt, val, n - 1)), solve(W, wt, val, n - 1));
-        }else{
-            return cost[n][W] = solve(W, wt, val, n - 1);
-        }
-
-
+    
+    
+    static int helper(int W, int wt[], int val[], int n) 
+    {
+        if(n==0 || W==0 ) return 0;
+         
+         if(dp[W][n]!=-1) return dp[W][n];
+         
+         if(W>=wt[n-1])
+         {
+             dp[W][n]= Math.max(helper(W-wt[n-1],wt,val,n-1)+val[n-1], helper(W,wt,val,n-1));
+             return dp[W][n];
+         }
+         else 
+         {
+             dp[W][n]=helper(W,wt,val,n-1);
+             return dp[W][n];
+         }
     }
+    
 }
 
 
