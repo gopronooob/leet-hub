21f3d674fcd98c403018d90e9439c38988031a5e
Akshay Pendbhaje <akshay.pendbhaje@gmail.com>
2022-10-27 10:31:15 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -53,32 +53,36 @@ class Solution
     static int knapSack(int W, int wt[], int val[], int n) 
     { 
         
-           dp =  new int[W+1][n+1]; 
-        Arrays.stream(dp).forEach(a -> Arrays.fill(a, -1));
-        return helper(W,wt,val,n);
+           dp =  new int[n+1][W+1];  
+           
+           for(int i=0;i<=W;i++)
+           {
+               dp[0][i]=0;
+           }
+           
+           for(int i=0;i<=n;i++)
+           {
+               dp[i][0]=0;
+           }
+           
+           for(int i=1;i<=n;i++)
+           {
+               for(int j=1;j<=W;j++)
+               {
+                   if(wt[i-1]<=j)
+                   {
+                       dp[i][j]=Math.max(dp[i-1][j],dp[i-1][j - wt[i - 1]]+val[i-1]);
+                   }
+                   else
+                   {
+                       dp[i][j]=dp[i-1][j];
+                   }
+               }
+           }
   
-         
+            return dp[n][W];
     } 
-    
-    
-    static int helper(int W, int wt[], int val[], int n) 
-    {
-        if(n==0 || W==0 ) return 0;
-         
-         if(dp[W][n]!=-1) return dp[W][n];
-         
-         if(W>=wt[n-1])
-         {
-             dp[W][n]= Math.max(helper(W-wt[n-1],wt,val,n-1)+val[n-1], helper(W,wt,val,n-1));
-             return dp[W][n];
-         }
-         else 
-         {
-             dp[W][n]=helper(W,wt,val,n-1);
-             return dp[W][n];
-         }
-    }
-    
+     
 }
 
 
