8195dc69fc42f4e2abe8be7c4cdeb7f0c5903ad3
Akshay Pendbhaje <akshay.pendbhaje@gmail.com>
2022-12-19 23:12:58 +0530

    Time: 2 ms (72.96%), Space: 74.2 MB (29.34%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,43 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        
         int globalMax=Integer.MIN_VALUE;
         int localMax=0;
-        
-//         for(int i=0;i<nums.length;i++)
-//         {
-//              localMax += nums[i];
-//              if(globalMax<localMax)
-//              {
-//                  globalMax=localMax;
-//              }
-//             localMax=Math.max(0,localMax);
-                 
-//         }
-//         return globalMax;
-        
-        int[][] dp= new int[nums.length+1][2];
-        Arrays.stream(dp).forEach(a -> Arrays.fill(a, -1));
-        return helper(nums,0,0,dp);
-        
-    }
-    
-    public static int helper(int[] nums,int index,int flag,int[][] dp)
-    {
-        if(nums.length<=index)
+        for(int i=0;i<nums.length;i++)
         {
-            return (flag==1)?0: Integer.MIN_VALUE ;
+            localMax+=nums[i];
+            globalMax=Math.max(localMax,globalMax);
+            localMax=Math.max(0,localMax);                
+            
         }
-        
-        if(dp[index][flag]!=-1) return dp[index][flag];
-        
-        if(flag==1)
-        {
-            return dp[index][flag]=Math.max(helper(nums,index+1,1,dp)+nums[index],0);
-        }
-        return dp[index][flag]=Math.max(helper(nums,index+1,1,dp)+nums[index],helper(nums,index+1,0,dp)); 
+        return globalMax;
     }
-    
-     
-       
-    
 }
