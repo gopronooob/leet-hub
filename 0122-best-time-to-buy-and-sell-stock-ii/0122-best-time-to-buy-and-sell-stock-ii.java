b9629e7c50e7b94c7e663e957e1938d4c5bc1529
Akshay Pendbhaje <akshay.pendbhaje@gmail.com>
2023-01-18 23:30:45 +0530

    Time: 1 ms (90.69%), Space: 42.1 MB (89.95%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,48 +1,21 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices.length<=1)
-            return 0;
-        int prev=0;
-        int next=1;
-        int profit=0; 
-        while(next<prices.length)
-        {
-            if(prices[prev]>prices[next])
-            {
-                prev++;
-                next++;     
-                continue;
-            }
-              
-                profit+=prices[next]-prices[prev];
-                prev++;
-                next++;  
-        }       
-        return profit;
-    }/*
-class Solution {
-    public int maxProfit(int[] prices) {
-        
-        
         int profit=0;
         int buy=prices[0];
-        int sell=prices[0];
-        
+        int sell=0;
         for(int i=1;i<prices.length;i++)
         {
-            //System.out.println(prices[i]+" "+buy+ " "+sell);
-            if(prices[i]<buy)
+            if(buy>prices[i])
             {
                 buy=prices[i];
             }
-            if(prices[i]>buy)
+            else
             {
-                sell=prices[i];
-                profit+=sell-buy;
+                profit+=prices[i]-buy;
                 buy=prices[i];
             }
+                        
         }
         return profit;
     }
-}*/
 }
